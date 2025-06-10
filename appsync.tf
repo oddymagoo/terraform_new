d26764d37c23e1325accd86b6915eb59d4c293da
Todd Chapman <todd.chapman@energyq.com.au>
2025-06-10 15:39:24 +1000

    cognito basic configuration but needs work

--- appsync.tf
+++ appsync.tf
@@ -1,27 +1,31 @@
+/*
 resource "aws_appsync_graphql_api" "hz3_graphql" { #Error: creating AppSync GraphQL API (hz3-graphql): operation error AppSync: CreateGraphqlApi, https response error StatusCode: 400, RequestID: d21cc9e0-1730-4a29-9e16-7c6ceb29c8de, BadRequestException: User Pool Config is empty
   api_type             = "GRAPHQL"
   authentication_type  = "AMAZON_COGNITO_USER_POOLS"
   introspection_config = "ENABLED"
-  /*
+
+  name                 = "hz3-graphql"
+  query_depth_limit    = "0"
+  resolver_count_limit = "0"
+
+  visibility   = "GLOBAL"
+  xray_enabled = "true"
+
+  # This section needs to be confirmed before implementation
   log_config {
     #cloudwatch_logs_role_arn = "arn:aws:iam::340002054667:role/hazchat-3-dev-log-role"
     exclude_verbose_content  = "false"
     field_log_level          = "ERROR"
   }
-*/
-  name                 = "hz3-graphql"
-  query_depth_limit    = "0"
-  resolver_count_limit = "0"
 
-
-  /* create user pool before
+  # User pool needs to be configured 
   user_pool_config {
     aws_region     = "us-west-2"
     default_action = "ALLOW"
     user_pool_id   = "us-west-2_0moF8DLkf"
   }
-*/
 
-  visibility   = "GLOBAL"
-  xray_enabled = "true"
 }
+
+
+*/
---
+++ cognito.tf
@@ -0,0 +1,89 @@
+resource "aws_cognito_user_pool" "hz3_user_pool" {
+  /*
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "admin_only"
+      priority = "1"
+    }
+  }
+
+  admin_create_user_config {
+    allow_admin_create_user_only = "true"
+  }
+
+  deletion_protection = "INACTIVE"
+  */
+
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
+
+  mfa_configuration = "OFF"
+  name              = "hz3-local-user-pool"
+
+  password_policy {
+    minimum_length                   = "8"
+    password_history_size            = "0"
+    require_lowercase                = "true"
+    require_numbers                  = "true"
+    require_symbols                  = "true"
+    require_uppercase                = "true"
+    temporary_password_validity_days = "7"
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = "false"
+    mutable                  = "true"
+    name                     = "address"
+    required                 = "false"
+
+    string_attribute_constraints {
+      max_length = "256"
+      min_length = "1"
+    }
+  }
+
+  sign_in_policy {
+    allowed_first_auth_factors = ["PASSWORD"]
+  }
+
+
+  user_pool_tier = "LITE"
+
+  username_configuration {
+    case_sensitive = "false"
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+  }
+}
+
+
+resource "aws_cognito_user_pool_client" "hz3_user_pool_client" {
+  access_token_validity                         = "60"
+  allowed_oauth_flows                           = ["code"]
+  allowed_oauth_flows_user_pool_client          = "true"
+  allowed_oauth_scopes                          = ["aws.cognito.signin.user.admin", "email", "openid", "profile"]
+  auth_session_validity                         = "3"
+  #callback_urls                                 = ["hazchat://signin"]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "false"
+  explicit_auth_flows                           = ["ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
+  id_token_validity                             = "60"
+  #logout_urls                                   = ["hazchat://signout"]
+  name                                          = "prod"
+  prevent_user_existence_errors                 = "ENABLED"
+  refresh_token_validity                        = "30"
+  #supported_identity_providers                  = ["A_z_u_r_e_A_D"]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "days"
+  }
+
+  user_pool_id = aws_cognito_user_pool.hz3_user_pool.name
+
+}
